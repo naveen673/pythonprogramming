ef807581e75d0ab6b97702da6f14f4041518a8b0
naveen673 <naveenkumar.reddy13@gmail.com>
2021-05-18 20:12:43 +1000

    changed the file names in ascending order

---
+++ 4.practice 3.py
@@ -0,0 +1,8 @@
+#Practice 3: Write an infinite loop. A inifinte loop never ends. Condition is always true.
+sum=0
+# n = int(input("Enter the number for range: \n"))
+# for i in range(n):
+#     print(sum)
+#     #i=29
+#     sum=sum+i
+# print(sum)
---
+++ 7.exercise_1.py
@@ -0,0 +1,8 @@
+def sum(a,b,c):
+    return a+b+c
+
+
+a = float(input("Enter the first number for a: \n"))
+b = float(input("Enter the second number for b: \n"))
+c = float(input("Enter the third number for c: \n"))
+print(sum(a,b,c))
