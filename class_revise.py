212e42a511790eb098c85e7dc3ee36903b4cafd6
naveen673 <naveenkumar.reddy13@gmail.com>
2021-06-08 19:56:51 +1000

    revise file

---
+++ class_revise.py
@@ -0,0 +1,22 @@
+# print(type("a"))
+# print(type(5))
+# print(type(float(5)))
+# a=input("INSERT NUMBER")
+# print(type(a))
+#range 10 means 0 to 9
+# for i in range(7, 15):
+#     print(i)
+
+def sum(a, b):
+    c=a+b
+    return c
+
+def statement_print():
+    print("ITS WORKING")
+
+ statement_print()
+z=sum(5,6)
+y=sum(9,10)
+print(z)
+e= 7+z
+print(e)
