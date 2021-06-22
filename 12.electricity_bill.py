e85bdb828cec89f1594f36bc4ee170f60f1cdf12
naveen673 <naveenkumar.reddy13@gmail.com>
2021-06-22 22:19:50 +1000

    updated electricity_bill file

--- 12.electricity_bill.py
+++ 12.electricity_bill.py
@@ -6,3 +6,16 @@ Next 100 units                                              Rs 5 per unit
 After 200 units                                             Rs 10 per unit
 (For example if input unit is 350 than total bill amount is Rs2000)
 '''
+
+units=int(input("The number of units consumed: "))
+if(units<101):
+    price=0
+    print("Electricity bill is: $",price)
+elif(units>100 and units<201):
+    price=(units-100)*5
+    print("Electricity bill is: $",price)
+elif(units>200):
+    price=(units-200)*10+500
+    print("Electricity bill is: $",price)
+else:
+    print("Electricity bill is invalid")
