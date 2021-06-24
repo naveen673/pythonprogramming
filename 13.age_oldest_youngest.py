bbe8ae9a40024cd9143fc18e8875b9f84e034adf
naveen673 <naveenkumar.reddy13@gmail.com>
2021-06-24 19:12:05 +1000

    updated electricity_bill file 2

--- 13.age_oldest_youngest.py
+++ 13.age_oldest_youngest.py
@@ -13,3 +13,12 @@ elif(age3>age1 and age3>age2):
     print("Person 3 is OLDER than person 1 and person 2 and the age is ",age3)
 else:
     print("The age entered is invalid")
+
+if(age1<age2 and age1<age3):
+    print("Person 1 is YOUNGER than person 2 and person 3 and the age is ",age1)
+elif(age2<age1 and age2<age3):
+    print("Person 2 is YOUNGER than person 1 and person 3 and the age is ",age2)
+elif(age3<age1 and age3<age2):
+    print("Person 3 is YOUNGER than person 1 and person 2 and the age is ",age3)
+else:
+    print("The age entered is invalid")
